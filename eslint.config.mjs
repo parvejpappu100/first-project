e089ce2f251851eed825905bcfe703bf4dd28c57
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2025-02-27 03:28:56 +0600

    set up basic server

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error"
+    },
+  },
+  pluginJs.configs.recommended,eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [".node_modules/*"],
+  },
+];
